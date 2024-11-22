7dddb96dd81585e2f27ee74b71d89cd978125488
Murat Tunc <murat.tunc8558@gmail.com>
2024-11-23 00:11:05 +0300

    fix17

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Enable static export mode
-  output: 'export',
+ 
   
   // You can add any other configuration options you need here
 };
